7d7036a906a67cf5fc716984844b3f1a449f180c
reg4in <gunzof@gmx.de>
2014-01-02 23:49:26 +0100

    got it working (but in fact not looking good)

--- js/script.js
+++ js/script.js
@@ -17,3 +17,47 @@ function diffHours(date1, date2) {
 function diffDays(date1, date2) {
   return diffHours(date1, date2)/24;
 }
+
+function withS(number) {
+	if (Math.floor(number) == 1) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function __(id) {
+	return document.getElementById(id);
+}
+
+function display() {
+	now = new Date();
+	
+	seconds = Math.floor(diffSeconds(now, firstJan));
+	minutes =  Math.floor(diffMinutes(now, firstJan));
+	hours = Math.floor(diffHours(now, firstJan));
+	days = Math.floor(diffDays(now, firstJan));
+	
+	__('seconds').innerHTML = seconds;
+	__('label-seconds').innerHTML = 'second' + (withS(seconds)?'s':'') + ' or';
+	
+	__('minutes').innerHTML = minutes;
+	__('label-minutes').innerHTML = 'minute' + (withS(minutes)?'s':'') + ' or';
+	
+	__('hours').innerHTML = hours;
+	__('label-hours').innerHTML = 'hour' + (withS(hours)?'s':'') + ' or';
+	
+	__('days').innerHTML = days;
+	__('label-days').innerHTML = 'day' + (withS(days)?'s':'') + ' since';
+	
+}
+
+window.onload = function() {
+  lastYear = (new Date()).getFullYear() - 1;
+  firstJan = new Date(lastYear + 1, 0, 1)
+  
+  __('lastyear').innerHTML = lastYear;
+  
+  display();
+  window.setInterval(display, 1000);
+};

5aa9ae30ea1fbbf54642ecd843e9c0d773cdd976
reg4in <gunzof@gmx.de>
2014-01-02 23:33:31 +0100

    for github pages to work

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function diffMilliseconds(date1, date2) {
+  return date1.getTime() - date2.getTime();
+}
+
+function diffSeconds(date1, date2) {
+  return diffMilliseconds(date1, date2)/1000;
+}
+
+function diffMinutes(date1, date2) {
+  return diffSeconds(date1, date2)/60;
+}
+
+function diffHours(date1, date2) {
+  return diffMinutes(date1, date2)/60;
+}
+
+function diffDays(date1, date2) {
+  return diffHours(date1, date2)/24;
+}
